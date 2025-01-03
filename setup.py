8a2f05d74ea42a33121040e6289d9e54d11b858e
Tim Empringham <tim.empringham@live.ca>
2025-01-03 08:19:49 -0500

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="ndbcrealtime", 
     packages=["ndbcrealtime"],
-    version="0.0.4",
+    version="0.0.5",
     license='apache-2.0',
     author="Tim Empringham",
     author_email="tim.empringham@live.ca",
